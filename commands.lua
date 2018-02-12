b466f016fb298c9a5aabe9e3d54a12415964847f
harrycode <pacboy2004@gmail.com>
2018-02-12 18:47:42 +1000

    fixed antigravitee

--- commands.lua
+++ commands.lua
@@ -1,9 +1,19 @@
+gravityIsAnti = false
 minetest.register_chatcommand("antigravity", {
     func = function(name, param)
         local player = minetest.get_player_by_name(name)
-        player:set_physics_override({
-            gravity = 0.1 -- set gravity to 10% of its original value
+        if not gravityIsAnti then
+          player:set_physics_override({
+              gravity = 0.1 -- set gravity to 10% of its original value
                           -- (0.1 * 9.81)
-        })
+          })
+          gravityIsAnti = true
+        else
+          player:set_physics_override({
+              gravity = 1 -- set gravity to 10% of its original value
+                          -- (0.1 * 9.81)
+          })
+          gravityIsAnti = false
+        end
     end
 })
